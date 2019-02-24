59e6467c133c9dd9184caba886ea00e7b237bb64
Jeroen Geel <jeroen.geel@hotmail.com>
2019-02-24 17:46:59 +0100

    Removed old files.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
-#include "NeoPixelTest.h"
 #include "ws2812bTest.h"
 
 int main(int argc, char **argv){
 
-    //NeoPixelTest test;
     ws2812bTest wsTest;
 
     std::cout << "NEOPIXEL" << std::endl;
 
     while(true)
     {
-//        test.EffectsDemo();
         wsTest.EffectsDemo();
     }
 
